60a9a263d3b89f673b92e9e3310207ea7ffbf26d
Kamil Bączkiewicz <kbaczkiewitz@gmail.com>
2017-09-13 18:16:17 +0200

    Project start

---
+++ README.md
@@ -0,0 +1,4 @@
+wiersze
+=======
+
+A Symfony project created on September 13, 2017, 6:05 pm.
---
+++ src/AppBundle/Entity/Background.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use AppBundle\Entity\Poem;
+/**
+ * Background
+ *
+ * @ORM\Table(name="background")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\BackgroundRepository")
+ */
+class Background
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="backgroundUrl", type="string", length=255)
+     */
+    private $backgroundUrl;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="backgroundColor", type="string", length=6)
+     */
+    private $backgroundColor;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fontColor", type="string", length=6)
+     */
+    private $fontColor;
+
+    /**
+     * @var Poem
+     * @ORM\OneToOne(targetEntity="AppBundle\Entity\Poem")
+     */
+    private $poem;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set backgroundUrl
+     *
+     * @param string $backgroundUrl
+     *
+     * @return Background
+     */
+    public function setBackgroundUrl($backgroundUrl)
+    {
+        $this->backgroundUrl = $backgroundUrl;
+
+        return $this;
+    }
+
+    /**
+     * Get backgroundUrl
+     *
+     * @return string
+     */
+    public function getBackgroundUrl()
+    {
+        return $this->backgroundUrl;
+    }
+
+    /**
+     * Set backgroundColor
+     *
+     * @param string $backgroundColor
+     *
+     * @return Background
+     */
+    public function setBackgroundColor($backgroundColor)
+    {
+        $this->backgroundColor = $backgroundColor;
+
+        return $this;
+    }
+
+    /**
+     * Get backgroundColor
+     *
+     * @return string
+     */
+    public function getBackgroundColor()
+    {
+        return $this->backgroundColor;
+    }
+
+    /**
+     * Set fontColor
+     *
+     * @param string $fontColor
+     *
+     * @return Background
+     */
+    public function setFontColor($fontColor)
+    {
+        $this->fontColor = $fontColor;
+
+        return $this;
+    }
+
+    /**
+     * Get fontColor
+     *
+     * @return string
+     */
+    public function getFontColor()
+    {
+        return $this->fontColor;
+    }
+
+    /**
+     * Set poem
+     *
+     * @param Poem $poem
+     *
+     * @return Background
+     */
+    public function setPoem(Poem $poem = null)
+    {
+        $this->poem = $poem;
+
+        return $this;
+    }
+
+    /**
+     * Get poem
+     *
+     * @return Poem
+     */
+    public function getPoem()
+    {
+        return $this->poem;
+    }
+}
